47bf7cf910091ae831f509911fb9c3b2a68e9fe3
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2018-02-17 16:29:31 -0800

    Basic MySQL database interactions implemented.

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+    public GameObject mainMenuCanvas;
+
+    InputField gameIDinputField;
+    Text errorText;
+
+    void Start()
+    {
+        gameIDinputField = mainMenuCanvas.transform.Find("GameIDInputField").GetComponent<InputField>();
+        errorText = mainMenuCanvas.transform.Find("ErrorText").GetComponent<Text>();
+        errorText.text = "";
+    }
+
+    public void OnButtonCreateGame()
+    {
+        StartCoroutine(CreateGame());
+    }
+
+    public void OnButtonJoinGame()
+    {
+        string gameID = gameIDinputField.text;
+
+        if (gameID == "")
+        {
+            errorText.text = "Error: no game ID entered!";
+            return;
+        }
+        else
+        {
+            errorText.text = "";
+            StartCoroutine(JoinGame(gameID));
+        }
+    }
+
+    IEnumerator CreateGame()
+    {
+        WWW newGameRequest = new WWW("http://localhost/sb/createNewGame.php");
+
+        yield return newGameRequest;
+
+        if (newGameRequest.error == null)
+        {
+            Debug.Log("New game created! " + newGameRequest.text);
+        }
+        else
+        {
+            Debug.LogError("Error: could not create new game.\n" + newGameRequest.error);
+        }
+    }
+
+    IEnumerator JoinGame(string gameID)
+    {
+        WWWForm gameJoinID = new WWWForm();
+        gameJoinID.AddField("gID", gameID);
+
+        WWW attemptGameJoin = new WWW("http://localhost/sb/joinGame.php", gameJoinID);
+
+        yield return attemptGameJoin;
+
+        if (attemptGameJoin.error == null)
+        {
+            Debug.Log("Joining game with ID: " + gameID);
+            Debug.Log("From Server: " + attemptGameJoin.text);
+        }
+        else
+        {
+            Debug.LogError("Error: could not join game with ID: " + gameID + "\nError from PHP script: " + attemptGameJoin.error);
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/TestDatabase.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestDatabase : MonoBehaviour {
+
+    public GameObject dataCanvas;
+
+    InputField inputDataField;
+    Text outputDataText;
+
+    void Start()
+    {
+        inputDataField = dataCanvas.transform.Find("InputData").GetComponent<InputField>();
+        outputDataText = dataCanvas.transform.Find("OutputData").GetComponent<Text>();
+    }
+
+    public void OnButton_SendData()
+    {
+        //Debug.Log("Send Data button has been pressed.");
+
+        string dataToSend = inputDataField.text;
+
+        Debug.Log("Sending '" + dataToSend + "' to server");
+
+        StartCoroutine(SendData(dataToSend));
+    }
+
+    public void OnButton_ReceiveData()
+    {
+        //Debug.Log("Receive Data button has been pressed.");
+
+        string dataReceived = inputDataField.text;
+
+        //Debug.Log("Mock receiving of string '" + dataReceived + "'");
+        //outputDataText.text = dataReceived;
+
+        StartCoroutine(ReceiveData());
+    }
+
+    IEnumerator SendData(string dataToSend)
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("name", dataToSend);
+        form.AddField("owner", "Jon");
+        form.AddField("species", "dawg");
+
+        WWW www = new WWW("http://localhost/testDataSend.php", form);
+
+        yield return www;
+
+        if (www.error == null)
+        {
+            Debug.Log("WWW Sent! " + www.text);
+        }
+        else
+        {
+            Debug.LogError("WWW Did not send! " + www.error);
+        }
+    }
+
+    IEnumerator ReceiveData()
+    {
+        WWW www = new WWW("http://localhost/testDataReceive.php");
+
+        yield return www;
+
+        if (www.error == null)
+        {
+            Debug.Log("WWW Received! " + www.text);
+        }
+        else
+        {
+            Debug.LogError("WWW did not receive! " + www.error);
+        }
+    }
+}
